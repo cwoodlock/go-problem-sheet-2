ff47f45f452e9877b92ca6e8fef6bd2eb185e0b0
cwoodlock <g00341460@gmit.ie>
2017-10-27 23:30:33 +0100

    4.2 tried to turn to template includes methods I tried

--- webApp.go
+++ webApp.go
@@ -1,8 +1,7 @@
 //Web application in golang problem sheet 2
 //Author: Colm Woodlock
 //Student Number: G00341460
-//Adapted from http://idealogylabs.com/blog/golang/2015/07/24/serving-static-files-with-golang-gorilla-mux.html
-
+//Adapted from: https://gowebexamples.com/templates/ and https://astaxie.gitbooks.io/build-web-application-with-golang/en/07.4.html
 package main
 
 import (
@@ -10,10 +9,30 @@ import (
 	"net/http"
 )
 
-func main() {
-	//This will serve the page using bootstrap to 127.0.0.1:8080/guess
-	index := http.StripPrefix("/guess", http.FileServer(http.Dir("index/")))
-	http.Handle("/guess", index)
+type Prompt struct {
+	Prompt string
+}
 
+func main() {
+	//Call the template handler
+	http.HandleFunc("/guess", TemplateHandler)
+
+	//Serve to 127.0.0.1:8080
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+//Function for the remplate handler
+func TemplateHandler(w http.ResponseWriter, r *http.Request) {
+
+	//********TRIED BOTH OF THESE WAYS COULDNT GET THEM TO WORK (compiles for both)*********
+
+	/*//Message to be used as the prompt
+	m := Prompt{Prompt: "Guess a number between 1 and 20"}
+	//parse from index.tmpl
+	t, _ := template.ParseFiles("guess/index.tmpl")
+	t.Execute(w, m)*/
+
+	/*gTemplate := template.Must(template.ParseFiles("guess/index.tmpl"))
+	//Merge
+	gTemplate.Execute(w, Message{Message: "Guess a number between 1 and 20"})*/
 }
